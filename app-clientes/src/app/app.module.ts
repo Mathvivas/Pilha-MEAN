7a8b0dd5b16348a0a49b03adb7ea9941638fd7df
Mathvivas <mathvivas@hotmail.com>
2021-04-24 10:19:34 -0300

    Criaçao da Inserçao do Cliente

--- app-clientes/src/app/app.module.ts
+++ app-clientes/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteInserirComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
