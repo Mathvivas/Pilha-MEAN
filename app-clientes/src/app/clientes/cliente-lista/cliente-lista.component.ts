aa2195a900a643cf986986d1ac7d5cca1144e923
Mathvivas <mathvivas@hotmail.com>
2021-05-08 10:46:32 -0300

    Fim do Front End e inicio do Back End

--- app-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ app-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
   styleUrls: ['./cliente-lista.component.css']
 })
-export class ClienteListaComponent implements OnInit {
+export class ClienteListaComponent implements OnInit, OnDestroy {
 
   clientes: Cliente[] = []
+  private clienteSubscription: Subscription
 
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
     this.clientes = this.clienteService.getClientes()
+    this.clienteService.getListaDeClientesAtualizadaObservable().subscribe((clientes: Cliente[]) => {
+      this.clientes = clientes
+    })
   }
 
+  ngOnDestroy(): void {
+    this.clienteSubscription.unsubscribe()
+  }
 }
--- app-clientes/src/app/clientes/cliente.service.ts
+++ app-clientes/src/app/clientes/cliente.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Cliente } from "./cliente.model";
 
 @Injectable({providedIn: 'root'})
 export class ClienteService {
   private clientes: Cliente[] = []
+  private listaClientesAtualizada = new Subject <Cliente[]>()
 
   getClientes(): Cliente[] {
     return [...this.clientes]
@@ -11,5 +13,12 @@ export class ClienteService {
 
   adicionarCliente(nome: string, fone: string, email: string) {
     const cliente: Cliente = { nome, fone, email }
+    this.clientes.push(cliente)
+    // clientes = [1, 2, 3] => ...clientes = 1, 2, 3 -> Extrai os elementos da lista
+    this.listaClientesAtualizada.next([...this.clientes])   // Extraiu os elementos e colocou em outra lista (cópia)
+  }
+
+  getListaDeClientesAtualizadaObservable() {
+    return this.listaClientesAtualizada.asObservable()
   }
 }
